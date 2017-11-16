0478ffd32f3e5dea0b0a95b251c080708371f586
Casper Panah <casperpanah@caspers-mini.mynet>
2017-11-16 13:11:49 +0100

    create account

--- Smack/Controller/CreateAccountVC.swift
+++ Smack/Controller/CreateAccountVC.swift
@@ -33,9 +33,15 @@ class CreateAccountVC: UIViewController {
         guard let email = emailTxt.text , emailTxt.text != "" else {return}
         guard let pass = passTxt.text , passTxt.text != "" else {return}
         
-        AuthService.instance.registerUser(email: email, password: pass) { (success) in
+        AuthService.instance.registerUser(email: email, password: pass)
+            { (success) in
             if success {
-                print("registered user!")
+                AuthService.instance.loginUser(email: email, password: pass, completion: { (success) in
+                    if success {
+                        print("logged in user!")
+                        print(AuthService.instance.authToken)
+                    }
+                })
             }
         }
     }
