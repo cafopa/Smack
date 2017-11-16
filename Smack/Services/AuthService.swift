d0943567b66874acf43296d302f848735bccb638
Casper Panah <casperpanah@caspers-mini.mynet>
2017-11-16 19:49:18 +0100

    Lesson 12

--- Smack/Services/AuthService.swift
+++ Smack/Services/AuthService.swift
@@ -54,16 +54,16 @@ class AuthService {
         
         Alamofire.request(URL_REGISTER, method: .post, parameters: body, encoding: JSONEncoding.default , headers: HEADER).responseString { (response) in
             
-            if response.result.error == nil {
-                guard let data = response.data else {return}
-                let json = JSON(data: data)
+            switch response.result {
+            case .success(let value):
+                let json = JSON(value)
                 self.userEmail = json["user"].stringValue
                 self.authToken = json["token"].stringValue
                 self.isLoggedIn = true
                 completion(true)
-            } else {
+            case .failure (let error):
                 completion(false)
-                debugPrint(response.result.error as Any)
+                debugPrint(error)
             }
         }
     }
@@ -75,26 +75,57 @@ class AuthService {
             "email": lowerCaseEmail,
             "password": password
         ]
-        
+    
         Alamofire.request(URL_LOGIN, method: .post, parameters: body, encoding: JSONEncoding.default, headers: HEADER).responseJSON { (response) in
             
             if response.result.error == nil {
-
+                guard let data = response.data else { return }
+                let json = JSON(data: data)
+                self.userEmail = json["user"].stringValue
+                self.authToken = json["token"].stringValue
+                
                 self.isLoggedIn = true
                 completion(true)
             } else {
                 completion(false)
-                debugPrint(response.error as Any)
+                debugPrint(response.result.error as Any)
             }
         }
-        
     }
-    
-    
-    
-    
-    
-    
-    
-    
+    func createUser(name: String, email: String, avatarName: String, avatarColor: String, completion: @escaping CompletionHandler) {
+        
+        let lowerCaseEmail = email.lowercased()
+        
+        let body: [String: Any] = [
+            "name": name,
+            "email": lowerCaseEmail,
+            "avatarName": avatarName,
+            "avatarColor": avatarColor
+        ]
+        
+        let header = [
+            "Authorization":"Bearer \(AuthService.instance.authToken)",
+            "Content-Type": "application/json; charset=utf-8"
+        ]
+        
+        Alamofire.request(URL_USER_ADD, method: .post, parameters: body, encoding: JSONEncoding.default, headers: header).responseJSON { (response) in
+            
+            if response.result.error == nil {
+                guard let data = response.data else { return }
+                let json = JSON(data: data)
+                let id = json["_id"].stringValue
+                let color = json["avatarColor"].stringValue
+                let avatarName = json["avatarName"].stringValue
+                let email = json["email"].stringValue
+                let name = json["name"].stringValue
+                
+                UserDataService.instance.setUserData(id: id, color: color, avatarName: avatarName, email: email, name: name)
+                completion(true)
+                
+            } else {
+                completion(false)
+                debugPrint(response.result.error as Any)
+            }
+        }
+    }
 }
